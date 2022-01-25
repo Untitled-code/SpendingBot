8dbd7ebb8372cdab48cde53f00e91e9b0b31f38b
Untitled-code <oganov.oleg@gmail.com>
2022-01-25 14:38:45 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/home/investigator/anaconda3/envs/bots_chat/bin/python
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
